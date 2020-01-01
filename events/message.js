1c709cc76251411b2f75346b1148757512bf517f
badplye-minelist <57844182+badplye-minelist@users.noreply.github.com>
2020-01-01 10:16:38 -0600

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,7 +1,9 @@
 const kick = require("../commands/kick");
 
 module.exports = (client, message) => {
-  if (message.content.startsWith("a!kick")) && (member.roles.some(role => role.name === 'Mod')) {
-    return kick(message);
+  if (message.content.startsWith("a!kick")) {
+    if(message.member.roles.some(r=>["Dev", "Mod", "Server Staff", "Proficient"].includes(r.name)) ) {
+      return kick(message);
+    }
   }
 };
