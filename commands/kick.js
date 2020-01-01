9b3c0e00d8c2ee74da9ed54f678fc3cc86cae9b4
badplye-minelist <57844182+badplye-minelist@users.noreply.github.com>
2020-01-01 10:54:08 -0600

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -2,17 +2,27 @@ module.exports = message => {
   const member = message.mentions.members.first();
 
   if (!member) {
-    return message.reply(
-      `Who are you trying to kick? You must mention a user.`
-    );
+    return message.channel.send({embed: {
+  color: 3447003,
+  description: "Who are you trying to kick? You must mention a member!"
+}});
   }
 
   if (!member.kickable) {
-    return message.reply(`I can't kick this user. Sorry!`);
+    return message.channel.send({embed: {
+  color: 3447003,
+  description: "I cant kick this user! Sorry."
+}});
   }
 
   return member
     .kick()
-    .then(() => message.reply(`${member.user.tag} was kicked.`))
-    .catch(error => message.reply(`Sorry, an error occured.`));
+    .then(() => message.channel.send({embed: {
+  color: 3447003,
+  description: "${member.user.tag} was kicked."
+}}))
+    .catch(error => message.channel.send({embed: {
+  color: 3447003,
+  description: "Sorry, an error occured."
+}}));
 };
