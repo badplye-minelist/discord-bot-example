957e43594eb28f4acc285973caab10fae04d69da
badplye-minelist <57844182+badplye-minelist@users.noreply.github.com>
2020-01-01 10:37:00 -0600

    Update message.js

--- events/message.js
+++ events/message.js
@@ -5,5 +5,19 @@ module.exports = (client, message) => {
     if(message.member.roles.some(r=>["Dev", "Mod", "Server Staff", "Proficient"].includes(r.name)) ) {
       return kick(message);
     }
+  } else { 
+    message.channel.send({embed: {
+    color: 0xFF8C8C,
+    fields: [{
+        name: "No Permission",
+        value: "You need to be a Moderator to execute this command"
+      },
+      {
+        name: "Additional Features",
+        value: "There are countless features we are working to add!"
+      }
+    ],
+  }
+});
   }
 };
