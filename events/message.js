de1a1af21ba041d90eb035bee5ad990b0c4a2c1e
badplye-minelist <57844182+badplye-minelist@users.noreply.github.com>
2020-01-01 10:42:41 -0600

    Update message.js

--- events/message.js
+++ events/message.js
@@ -4,6 +4,11 @@ module.exports = (client, message) => {
   if (message.content.startsWith("a!kick")) {
     if(message.member.roles.some(r=>["Dev", "Mod", "Server Staff", "Proficient"].includes(r.name)) ) {
       return kick(message);
+    } else {
+      message.channel.send({embed: {
+  color: 3447003,
+  description: "You do not have permission to execute this command!"
+}});
     }
   }
 };
